894d8c23331093a4b8f144d05159d90b33b9426f
Louis-Joachin <joachinmessie@gmail.com>
2022-03-14 11:49:05 +0100

    oui bonsoir

--- projet.py
+++ projet.py
@@ -1,1 +1,17 @@
-print("hello baptou")
+import os,sys
+import csv
+from Pillow import Image
+
+
+with open('label.csv','w',newline='') as csvfile :
+    label = csv.writer(csvfile, delimiter=',')
+    for dossier in os.listdir('./images'):
+        for image in os.listdir('./images/'+dossier):
+            label.writerow(['./images/'+dossier+'/'+image]+[dossier])
+
+
+
+        
+
+
+

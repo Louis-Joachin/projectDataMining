acf300c4b1ebca7af15afe7c1f492520bacdc6de
Louis-Joachin <joachinmessie@gmail.com>
2022-03-28 23:11:18 +0200

    fin de session

--- analyse.py
+++ analyse.py
@@ -5,16 +5,9 @@ import numpy
 from sklearn.cluster import MiniBatchKMeans
 import math
 
-jsonTab=open("label.json",'r')
-dataTab = json.load(jsonTab)
-jsonTab.close()
-dataframeTab = json_normalize(dataTab)
-
-jsonUser=open("user.json",'r')
-dataUser = json.load(jsonUser)
-jsonUser.close()
-dataframeUser = json_normalize(dataUser)
-
+#Fonction de comparaison de couleurs
+#Entrées : deux codes RGB de deux couleurs
+#Sorties : un indice de proximité des couleurs
 def ComparaisonCouleur(couleur1, couleur2):
     dif = []
     for i in range (3):
@@ -22,6 +15,18 @@ def ComparaisonCouleur(couleur1, couleur2):
     #mise au carré pour être sûr d'avoir des valeurs de diférence positive 
     return dif[0]**2+dif[1]**2+dif[2]**2
 
+
+#ouverture des fichiers précédemment créés
+with open("label.json",'r') as jsonTab:
+    dataTab = json.load(jsonTab)
+dataframeTab = json_normalize(dataTab)
+
+with open("user.json",'r') as jsonUser:
+    dataUser = json.load(jsonUser)
+dataframeUser = json_normalize(dataUser)
+
+
+#détermination des couleurs et des formats préférés de l'utilisateur en fonction des likes
 couleursPref = []
 listeFormat = []
 compteFormat = []
@@ -39,6 +44,7 @@ for tab in dataTab:
 formatPref=listeFormat[compteFormat.index(max(compteFormat))]
 dataUser[0]['formatPref']=formatPref
 
+#on utilise une fonction KMeans car une moyenne de toutes les couleurs résulterait en une couleur moyenne ~=[122,122,122]
 #partitionement des couleurs dominantes de tous les tableaux en 3 cluster
 couleursPref = MiniBatchKMeans(3).fit(couleursPref)
 #création d'un numpyarray
@@ -55,20 +61,16 @@ dataUser[0]['couleurPref']=[R,G,B]
 
 
 
-
+#détermination du tag préféré de l'utilsateur
 listeTag=[]
 compteTag=[]
 for tableau in dataUser[0]['tags']:
     tag=dataUser[0]['tags'][tableau]
-    
-    
     if tag in listeTag:
         compteTag[listeTag.index(tag)]+=1
     else:
         compteTag.append(1)
         listeTag.append(tag)
-
-
 
 tagPref=listeTag[compteTag.index(max(compteTag))]
 dataUser[0]['tagPref']=tagPref

ff585661e7cad257a94ab2e66f97aa970d5168e4
Louis-Joachin <joachinmessie@gmail.com>
2022-04-03 15:30:49 +0200

    fin recommandations

--- recommandations.py
+++ recommandations.py
@@ -20,7 +20,7 @@ def ComparaisonCouleur(couleur1, couleur2):
 
 #ouverture des fichiers précédemment créés
 with open("label.json",'r') as jsonTab:
-    dataTabCustom = json.load(jsonTab)
+    dataTab = json.load(jsonTab)
 
 with open("user.json",'r') as jsonUser:
     dataUser = json.load(jsonUser)
@@ -29,18 +29,18 @@ with open("user.json",'r') as jsonUser:
 data=[]
 result=[]
 auteur_vu=[]
-dataTab = dataTabCustom
-for lien in dataUser[0]["likes"]:
+dataTabCustom = dataTab #on crée une copie de notre liste de tableaux, qui représentera les tableaux "non vus" par l'utilisateur
+for lien in dataUser[0]["likes"]: #on construit un dataframe qui va servir à l'algo de classification : on commence par les tableaux likés par l'utilisateur (result = 1)
     for tableau in dataTab:
         if lien == tableau["lien"]:
-            tag = max(tableau["tags"].items(), key=operator.itemgetter(1))[0]
+            tag = max(tableau["tags"].items(), key=operator.itemgetter(1))[0] #l'algo ne prend pas de liste de tags en paramètre: on choisit donc le tag le plus utilisé sur le tableau
             data.append([tableau["auteur"],tableau["format"],tag])
             result.append(1)
             auteur_vu.append(tableau["auteur"])
             dataTabCustom.remove(tableau)
             
 
-for lien in dataUser[0]["unlikes"]:
+for lien in dataUser[0]["unlikes"]:#on fait la même chose avec les tableaux vu et non likés (result = -1)
     for tableau in dataTab:
         if lien == tableau["lien"]:
             tag = max(tableau["tags"].items(), key=operator.itemgetter(1))[0]
@@ -51,10 +51,10 @@ for lien in dataUser[0]["unlikes"]:
 
 
 
-dataframe = pd.DataFrame(data, columns=['auteur', 'format', 'tags'])
+dataframe = pd.DataFrame(data, columns=['auteur', 'format', 'tags'])#construction des dataframes
 resultframe = pd.DataFrame(result, columns=['like'])
 
-le1 = LabelEncoder()
+le1 = LabelEncoder() #encodage des labels
 dataframe['auteur'] = le1.fit_transform(dataframe['auteur'])
 
 le2 = LabelEncoder()
@@ -66,24 +66,30 @@ dataframe['tags'] = le3.fit_transform(dataframe['tags'])
 le4 = LabelEncoder()
 resultframe['like'] = le4.fit_transform(resultframe['like'])
 
-#Use of decision tree classifiers
+#On utilise les classifieurs "arbres"
 dtc = tree.DecisionTreeClassifier()
 dtc = dtc.fit(dataframe, resultframe)
 
-recommandation = []
-non_recommandation=[]
+recommandation = [] #on initialise une liste de tableaux recommandés
+non_recommandation=[] #on initialise une liste de tableaux non-recommandés
 while len(recommandation)<10:
     tab = random.choice(dataTabCustom)
-    if tab["auteur"] not in auteur_vu:
+    if tab["auteur"] not in auteur_vu: #l'algo de prédiction plante si on lui présente un tableau dont l'auteur est n'a pas été vu par l'utilisateur.
         continue
-    prediction = dtc.predict([
+    prediction = dtc.predict([ #on réalise une prédiction pour savoir si le tableau plaira à l'utilisateur
             [le1.transform([tab["auteur"]])[0], le2.transform([tab["format"]])[0],
             le3.transform([max(tableau["tags"].items(), key=operator.itemgetter(1))[0]])[0]]])
-    if prediction == 1:
+    if prediction == 1: 
         recommandation.append(tab)
     else:
         non_recommandation.append(tab)
 
-for tab in recommandation:
+for tab in recommandation: #on ajoute une comaraison par couleurs des tableaux, afin d'avoir la recommandation la plus précise possible
     tab["indice_couleur"]=ComparaisonCouleur(dataUser[0]["couleurPref"],tab["couleur"])
 
+dataframe=pd.DataFrame(recommandation)
+dataframe.drop(columns=["auteur",'largeur','hauteur','format','tags','unlikes','couleur'],inplace=True)
+dataframe.sort_values(by="indice_couleur",inplace=True)
+if dataframe.iloc[0]['indice_couleur']>200 :
+    dataframe.sort_values(by="likes",inplace=True)
+print(dataframe.iloc[0]['lien'])
